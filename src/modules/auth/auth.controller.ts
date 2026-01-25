ef23779e5d8b9dd1f2585b8a02cc736a3e30a18a
ezc9-tech <ezc9@outlook.com>
2026-01-24 18:55:35 -0600

    This sets up the ability to get your own profile with the bearer token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { registerUser, loginUser } from "./auth.service";
+import { prisma } from "../../config/db";
+import { AuthRequest } from "../../middlewares/auth.middleware";
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -20,3 +22,29 @@ export const login = async (req: Request, res: Response) => {
         res.status(400).json({message: error.message});
     }
 }
+
+
+export const me = async (req: AuthRequest, res: any) => {
+  try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+
+    res.json(user);
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { register, login } from "./auth.controller";
+import { register, login, me } from "./auth.controller";
+import { requireAuth } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login)
+router.get("/me", requireAuth, me)
 
 export default router
